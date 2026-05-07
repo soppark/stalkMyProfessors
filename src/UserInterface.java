2424b7e2d13b557dcfa3a3ca44545b87cbf3d213
Max Liu <pose8189@outlook.com>
2026-05-07 11:50:24 -0700

    DeptFinder added and userinterface updated

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -3,20 +3,38 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class UserInterface {
+    public static void getProf(Scanner scanner,String data) throws IOException{
+        System.out.print("Enter the name of the Professor you'd like to learn more about: ");
+        String profName=scanner.nextLine();
+        
+        HashMap<String,Prof> map=new ProfList(data).getHashMap();
+        if(map.containsKey(profName)){
+            System.out.println(map.get(profName).toStringDetails());
+        }else{
+            System.out.println("No prof with this name.");
+        }
+    }
+
+    public static void getDept(Scanner scanner,String data) throws IOException{
+        System.out.print("Enter the department you'd like to learn more about: ");
+        String deptName=scanner.nextLine();
+        
+        HashMap<String,Prof> map=new ProfList(data).getHashMap();
+        //HERE
+    }
+
     public static void main(String[] args) throws IOException {
         try (Scanner scanner = new Scanner(System.in)) {
             String data="data/all_output.csv";
 
-            System.out.print("Enter the name of the Professor you'd like to learn more about: ");
-            String profName=scanner.nextLine();
-            
-            HashMap<String,Prof> map=new ProfList(data).getHashMap();
-            if(map.containsKey(profName)){
-                System.out.println(map.get(profName).toStringDetails());
-            }else{
-                System.out.println("No prof with this name.");
+            while (true) { 
+                System.out.print("Enter 1 to search Prof, 2 for Dept, 3 for graph");
+                String c = scanner.nextLine();
+                if(c.equals("1")) getProf(scanner,data);
+                if(c.equals("2")) getDept(scanner);
+                
+                //Name has to be exact match with data base, not ideal
             }
-            //Name has to be exact match with data base, not ideal
         }
     }
 }
